77b32dc0293992a721e9aa49fb52216fa0fba7d3
tanishqacodes <tanishqasharma797@gmail.com>
2023-08-01 13:16:37 +0530

    assemble-gather together

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret = "mysitesessionsecret";
+
+const emailUser = "tanishqasharma797@gmail.com";
+const emailPassword = "gdzzsefcxsgglwdp";
+
+module.exports ={
+   sessionSecret,
+   emailUser,
+   emailPassword
+}
